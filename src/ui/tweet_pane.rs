66f81ec605f7e804ac086a8c585209f52ed304cc
Andrew Lee <665282+chairmanlee8@users.noreply.github.com>
2023-02-17 09:48:30 -0300

    line break using textwrap

--- src/ui/tweet_pane.rs
+++ src/ui/tweet_pane.rs
@@ -4,11 +4,9 @@ use anyhow::Result;
 use crossterm::event::KeyEvent;
 use crossterm::terminal::{self, ClearType};
 use crossterm::{cursor, queue, style};
-use regex::Regex;
 use std::collections::HashMap;
 use std::io::Stdout;
 use std::sync::{Arc, Mutex};
-use unicode_segmentation::UnicodeSegmentation;
 
 pub struct TweetPane {
     tweets: Arc<Mutex<HashMap<String, api::Tweet>>>,
@@ -38,7 +36,6 @@ impl Render for TweetPane {
         } = bounding_box;
 
         if let Some(tweet_id) = &self.selected_tweet_id {
-            let re_newlines = Regex::new(r"[\r\n]+").unwrap();
             let str_unknown = String::from("[unknown]");
 
             let tweets = self.tweets.lock().unwrap();
@@ -47,18 +44,9 @@ impl Render for TweetPane {
             let tweet_author_username = tweet.author_username.as_ref().unwrap_or(&str_unknown);
             let tweet_author_name = tweet.author_name.as_ref().unwrap_or(&str_unknown);
             let tweet_author = format!("@{tweet_author_username} [{tweet_author_name}]");
+            let tweet_lines = textwrap::wrap(&tweet.text, width.saturating_sub(1) as usize);
 
             let mut row = top;
-
-            // CR: graphemes is one thing but should split on words then greedy instead
-            // CR: some graphemes are double width, need to count correctly
-            // CR-someday: use Knuth algorithm
-            let tweet_paragraphs: Vec<&str> = re_newlines.split(&tweet.text).collect();
-            let tweet_lines: Vec<String> = tweet_paragraphs
-                .iter()
-                .map(|p| break_lines(p, (width - 1) as usize))
-                .flatten()
-                .collect();
 
             queue!(stdout, cursor::MoveTo(left, row))?;
             queue!(stdout, terminal::Clear(ClearType::UntilNewLine))?;
@@ -98,26 +86,19 @@ impl Input for TweetPane {
     }
 }
 
-fn break_lines(text: &str, line_width: usize) -> Vec<String> {
-    // CR: why does this work?
-    UnicodeSegmentation::graphemes(text, true)
-        .collect::<Vec<&str>>()
-        .chunks(line_width)
-        .map(|chunk| chunk.concat())
-        .collect::<Vec<String>>()
-}
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn test_segmentation() {
-        let str = "Why did the chicken cross the road? Because he wanted to get to the other side.";
+        // NB: expectation is correct; reasoning may be subtle
+        let str = "Why did the chicken cross the road?\n\nBecause he wanted to get to the other side.";
         let result = textwrap::wrap(str, 20);
         assert_eq!(
             result,
             vec![
                 "Why did the chicken",
                 "cross the road?",
+                "",
                 "Because he wanted",
                 "to get to the other",
                 "side."
